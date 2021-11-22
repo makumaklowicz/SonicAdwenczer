4db2d2dcd8948999b30065d68ccb5b1d2c50df78
makumaklowicz <mistermaniuspl@o2.pl>
2021-11-22 22:35:26 +0100

    Proper gravity and movement for sonic

--- Scripts/PlayerMovement.gd
+++ Scripts/PlayerMovement.gd
@@ -1,8 +1,24 @@
 extends KinematicBody2D
 
+#Jump setup
+export var fall_gravity_scale := 150.0
+export var low_jump_gravity_scale := 1000.0
+export var jump_power := 1500.0
+var jump_released = false
 
+#Physics setup
+var velocity = Vector2()
+var earth_gravity = 9.807 # m/s^2
+export var gravity_scale := 400.0
+var on_floor = false
 
+#Movement speed setup
+export (int) var movement_speed = 700
+
+#Reference to animation node
 onready var _animated_sprite = $AnimatedSprite
+
+#Using animations
 func _process(_delta):
 	if Input.is_action_pressed("ui_right") || Input.is_action_pressed("ui_left") :
 		_animated_sprite.play("Run")
@@ -10,45 +26,41 @@ func _process(_delta):
 		_animated_sprite.play("Idle")
 
 
-export (int) var speed = 500
-var velocity = Vector2()
-export var fallMultiplier = 2 
-export var lowJumpMultiplier = 10 
-export var jumpVelocity = 400 #Jump height
-export var gravity = 8
-
+#Left/Right Movement
 func get_input():
-	velocity = Vector2()
+	velocity.x = 0;
 	if Input.is_action_pressed("ui_right"):
 		self.scale.x = self.scale.y * 1
 		velocity.x += 1
+
 	if Input.is_action_pressed("ui_left"):
 		self.scale.x = self.scale.y * -1
 		velocity.x -= 1
-	
 
-	velocity = velocity.normalized() * speed
+	velocity.x = velocity.x * movement_speed
 
+#Physics and jumping
 func _physics_process(delta):
 	get_input()
-	velocity = move_and_slide(velocity)
-	velocity.y += gravity 
+	if Input.is_action_just_released("ui_accept"):
+		jump_released = true
+
+	#Applying gravity to player
+	velocity += Vector2.DOWN * earth_gravity * gravity_scale * delta
 
 	#Jump Physics
-	if velocity.y > 0: #Player is falling
-		velocity += Vector2.UP * (-9.81) * (fallMultiplier) #Falling action is faster than jumping action | Like in mario
+	if velocity.y > 0: 
+		velocity += Vector2.DOWN * earth_gravity * fall_gravity_scale * delta 
 
-	elif velocity.y < 0 && Input.is_action_just_released("ui_up"): #Player is jumping 
-		velocity += Vector2.UP * (-9.81) * (lowJumpMultiplier) #Jump Height depends on how long you will hold key
+	elif velocity.y < 0 && jump_released: 
+		velocity += Vector2.DOWN * earth_gravity * low_jump_gravity_scale * delta
 
-	if is_on_floor():
-		if Input.is_action_just_pressed("ui_up"): 
-			velocity = Vector2.UP * jumpVelocity #Normal Jump action
-	velocity = move_and_slide(velocity, Vector2(0,-1))  
+	if on_floor:
+		if Input.is_action_just_pressed("ui_accept"): 
+			velocity = Vector2.UP * jump_power 
+			jump_released = false
+
+	velocity = move_and_slide(velocity, Vector2(1,-1)) 
 	
-
-
-
-
- 
-	
+	if is_on_floor(): on_floor = true
+	else: on_floor = false
